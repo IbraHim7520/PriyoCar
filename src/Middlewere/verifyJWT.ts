ea0584fc9eb00f68b758da44d80fe44fa8de1516
Ibrahim752006 <mdibrahim752006@gmail.com>
2025-12-10 03:33:51 +0600

    token based authorization

--- src/Middlewere/verifyJWT.ts
+++ src/Middlewere/verifyJWT.ts
@@ -1,17 +1,27 @@
 import { NextFunction, Request, Response } from "express";
-import jwt from "jsonwebtoken"
+import jwt, { JwtPayload } from "jsonwebtoken"
 import envConfig from "../Configs/config";
-const verfyAuthorization = async(req:Request , res:Response , next:NextFunction) =>{
-    const token = req.headers.authorization?.split(" ")[1];
-    if(!token){
-        return res.status(401).send({
-            message: "Unauthorized access!"
-        })
-    }
+import { pool } from "../Configs/dbconfig";
 
-    jwt.verify(token , envConfig.jwt_secret, (err, decoded)=>{
-        console.log(decoded)
-    })
-    
+const verfyAuthorization = async(...roles:string[]) =>{
+    return async(req:Request , res:Response, next:NextFunction)=>{
+        const token = req.headers.authorization?.split(" ")[1];
+        if(!token){
+            return res.status(401).send({message: "Unauthorized Access!"})
+        }
+        const decoded = jwt.verify(token , envConfig.jwt_secret) as JwtPayload;
+        const {name , email , role} = decoded;
+
+        const dbUser = await pool.query(`SELECT * FROM Users WHERE email = $1`, [email]);
+        if(dbUser.rows.length<=0){
+            return res.status(404).send({
+                message: "User not found! Please Register."
+            })
+        }
+        if(roles.length && !roles.includes(role)){
+            return res.status(401).send({message: "Unauthorized user!"})
+        }
+        next()
+    }
 }
 export default verfyAuthorization
--- src/Modules/Users/user.routes.ts
+++ src/Modules/Users/user.routes.ts
@@ -7,7 +7,7 @@ const userRouter = express.Router();
 
 
 userRouter.post("/" , userControllers.registerUser);
-userRouter.get("/", verfyAuthorization , userControllers.getAllusers)
+userRouter.get("/",  userControllers.getAllusers)
 userRouter.put("/:userId" ,userControllers.updateUser);
 userRouter.delete("/:userId", userControllers.deleteUser);
 
